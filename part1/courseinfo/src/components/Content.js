8b7030a2bd52b9ec687bc3262ea02a983bc63b8e
Sandesh Hyoju <sundesz@gmail.com>
2021-12-01 00:36:31 +0200

    Add solution to exercise part1 - 1.3

--- part1/courseinfo/src/components/Content.js
+++ part1/courseinfo/src/components/Content.js
@@ -1,19 +1,12 @@
 import React from 'react'
 import Part from './Part'
 
-const Content = ({
-  part1,
-  part2,
-  part3,
-  exercises1,
-  exercises2,
-  exercises3,
-}) => {
+const Content = ({ part1, part2, part3 }) => {
   return (
     <div>
-      <Part part={part1} exercises={exercises1} />
-      <Part part={part2} exercises={exercises2} />
-      <Part part={part3} exercises={exercises3} />
+      <Part part={part1} />
+      <Part part={part2} />
+      <Part part={part3} />
     </div>
   )
 }
