e569e2dee513a272e9dadcf24c234c7ed6e025f7
Sandesh Hyoju <sundesz@gmail.com>
2021-12-01 01:52:54 +0200

    Add solution to exercise part1 - 1.7 unicafe

--- part1/unicafe/src/components/Statistics.js
+++ part1/unicafe/src/components/Statistics.js
@@ -3,12 +3,19 @@ import Header from './Header'
 import Info from './Info'
 
 const Statistics = ({ good, neutral, bad }) => {
+  const total = good + neutral + bad
+  const average = (good * 1 + neutral * 0 + bad * -1) / total
+  const positive = (good / total) * 100
+
   return (
     <>
       <Header title='statistics' />
       <Info name='good' value={good} />
       <Info name='neutral' value={neutral} />
       <Info name='bad' value={bad} />
+      <Info name='all' value={total} />
+      <Info name='average' value={average} />
+      <Info name='positive' value={`${positive} %`} />
     </>
   )
 }
