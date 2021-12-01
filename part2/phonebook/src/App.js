9c17b3064663f92f7517cbcc58078821c9f2661f
Sandesh Hyoju <sundesz@gmail.com>
2021-12-02 01:43:52 +0200

    Add solution to exercise part2 - 2.9 The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' },
-  ])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
-
-  const checkDuplicateName = () => {
-    return persons.map((p) => p.name).includes(newName)
-  }
+  const [searchText, setSearchText] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 },
+  ])
 
   const submitHandler = (e) => {
     e.preventDefault()
@@ -25,13 +25,26 @@ const App = () => {
     setNewNumber(() => '')
   }
 
+  const checkDuplicateName = () => persons.map((p) => p.name).includes(newName)
   const nameHandler = (e) => setNewName(() => e.target.value)
-
   const numberHandler = (e) => setNewNumber(() => e.target.value)
+  const searchHandler = (e) => setSearchText(() => e.target.value)
+
+  const filteredPersons =
+    searchText === ''
+      ? persons
+      : persons.filter((p) =>
+          p.name.toLowerCase().includes(searchText.toLowerCase())
+        )
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter shown with <input value={searchText} onChange={searchHandler} />
+      </div>
+
+      <h2>add a new</h2>
       <form onSubmit={submitHandler}>
         <div>
           name: <input value={newName} onChange={nameHandler} />
@@ -45,7 +58,7 @@ const App = () => {
       </form>
 
       <h2>Numbers</h2>
-      {persons.map((person) => (
+      {filteredPersons.map((person) => (
         <p key={person.name}>
           {person.name} {person.number}
         </p>
